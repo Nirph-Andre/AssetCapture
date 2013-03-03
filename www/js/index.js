3defb79ce3e7782b2a3284a11c0f0b3f5bae24ab
Andre Fourie <andre@nirph.com>
2013-03-03 20:46:02 +0200

    Prototype expansion

--- www/js/index.js
+++ www/js/index.js
@@ -49,6 +49,24 @@ var app = {
       app.stateDescription = description ? description : 'Ready';
       $('#appStatus').html(app.state);
       alert(app.stateDescription);
+    },
+    loadLayout: function(name) {
+      $('#contentLayout').html(data.view('content', {
+        'type': 'layout',
+        'name': name
+        }));
+    },
+    loadPage: function() {
+      $('#contentLayout').html(data.view('content', {
+        'type': 'page',
+        'name': name
+        }));
+    },
+    loadContent: function() {
+      $('#contentLayout').html(data.view('content', {
+        'type': 'content',
+        'name': name
+        }));
     }
 };
 
@@ -80,23 +98,55 @@ var tables = {
 };
 
 var data = {
+    // Database object
     db: null,
+    // Query counter
+    qC: 0,
+    // Query handling data
+    qCdata: {},
+    // Clean store template
+    storeClean: {},
+    // Data store
+    store: {},
+    
+    
+    // Initialize db handling
     initialize: function() {
       console.log('data.initialize()');
       app.setState('DbInit', 'Local database initializing.');
       data.openDatabase();
     },
+    
+    // Open db connection
     openDatabase: function() {
-      this.db = window.openDatabase(config.dbName, config.dbVersion, config.dbDisplayName, config.dbSize);
+      data.db = window.openDatabase(config.dbName, config.dbVersion, config.dbDisplayName, config.dbSize);
+      data.initStore();
       if (true) {
         data.db.transaction(data.resetDb, data.transactError, data.transactSuccess);
       } else {
         app.setState();
       }
     },
+    
+    // Initialize data store
+    initStore: function() {
+      var table = '';
+      for (table in tables) {
+        data.storeClean[table] = {};
+      }
+      data.store = data.storeClean;
+    },
+    
+    // Reset data store from template
+    cleanStore: function() {
+      data.store = data.storeClean;
+    },
+    
+    // ReCreate tables
     resetDb: function(tx) {
       var entity = '';
       var table = '';
+      var index = '';
       for (entity in entities) {
         table = entity;
         var field = '';
@@ -109,7 +159,8 @@ var data = {
         tx.executeSql('DROP TABLE IF EXISTS ' + table);
         tx.executeSql(stmnt);
         if (entities[entity]['children']) {
-          for (table in entities[entity]['children']) {
+          for (index in entities[entity]['children']) {
+            table = entities[entity]['children'][index];
             var field = '';
             var stmnt = 'CREATE TABLE IF NOT EXISTS ' + table + ' (id unique';
             for (field in tables[entity]) {
@@ -123,12 +174,121 @@ var data = {
         }
       }
     },
+    
+    // Success and error handling
     transactSuccess: function() {
       alert('data.transactSuccess()');
       app.setState();
     },
     transactError: function(err) {
-      alert('data.transactError: ' + err);
+      alert('data.transactError: ' + err.message);
       app.setState('DbError', 'Local database error');
+    },
+    querySuccess: function(tx, result) {
+      alert('data.querySuccess()');
+      data.store = result.rows;
+      app.setState();
+    },
+    queryError: function(err) {
+      alert('data.queryError: ' + err.message);
+      app.setState('DbError', 'Local database error');
+    },
+    
+    // Generic query execution
+    query: function(statement, success, fail) {
+      if (!success) {
+        return;
+      }
+      var errorCallback = fail ? fail : data.queryError;
+      data.db.transaction(function(tx) {
+        tx.executeSql(statement);
+      }, errorCallback, successCallback);
+    },
+    
+    // View a single record with relevant dependants
+    view: function(entity, id, where, success, fail) {
+      data.cleanStore();
+      var tmpStore = {};
+      // Prepare statement
+      var table = '';
+      var stmnt = 'SELECT * FROM ' + entity;
+      if (id) {
+        stmnt += ' WHERE id = ' + id;
+      }
+      if (where) {
+        var filter = [];
+        for (field in where) {
+          filter.push(field + ' = "' + where[index] + '"');
+        }
+        stmnt += ' WHERE ' + filter.join(' AND ');
+      }
+      // Execute query
+      alert(stmnt);
+      data.query(stmnt, function(tx, result) {
+        // Do we have data?
+        if (result.rows.length) {
+          data.store[entity] = result.rows.shift();
+          tmpStore = data.store[entity];
+          // Do we have dependant data to collect?
+          if (entities[entity]['children']) {
+            // Keep some meta so that we know when the stack is complete
+            var depTable = '';
+            var myQc = data.qC++;
+            if (data.qC > 500000) {
+              data.qC = 0; 
+            }
+            stmnt = [];
+            var index = 0;
+            for (index in entities[entity]['children']) {
+              depTable = entities[entity]['children'][index];
+              stmnt.push('SELECT "' + depTable + '" AS table_ident, ' + depTable + '.* FROM ' + depTable + ' WHERE id = ' + tmpStore[entity][depTable + '_id']);
+            }
+            data.qCdata[myQc] = {'counter': stmnt.length, 'handled': 0};
+            // Collect all dependant data
+            for (index in stmnt) {
+              alert(stmnt[index]);
+              data.query(stmnt[index], function(tx, result) {
+                if (result.rows.length) {
+                  var row = result.rows.shift();
+                  var tableIdent = row.table_ident;
+                  delete row.table_ident;
+                  data.store[tableIdent] = row;
+                  tmpStore[tableIdent] = row;
+                } else {
+                  data.store[tableIdent] = {};
+                  tmpStore[tableIdent] = {};
+                }
+                data.qCdata[myQc]['handled'] += 1;
+                if (data.qCdata[myQc]['counter'] == data.qCdata[myQc]['handled']) {
+                  // Last query result, do callback
+                  if (success) {
+                    success(tmpStore);
+                  }
+                }
+              }, function(err) {
+                // Oops, something went wrong
+                alert('data.queryError: ' + err.message);
+                if (fail) {
+                  fail(err);
+                }
+              });
+            }
+          }
+        } else {
+          // No entry found
+          data.store[entity] = {};
+          tmpStore = {};
+          if (success) {
+            success(tmpStore);
+          }
+        }
+      }, function(err) {
+        // Oops, something went wrong
+        alert('data.queryError: ' + err.message);
+        if (fail) {
+          fail(err);
+        }
+      });
+      
     }
 };
