5429c123df9fc596a4d150130e40aabbd00d7b0c
Andre Fourie <andre@nirph.com>
2013-06-13 19:13:11 +0200

    Changes

--- www/js/barcode.js
+++ www/js/barcode.js
@@ -20,6 +20,24 @@ var Barcode = {
       } catch(err) {
         Notify.alert('Oops', 'Could not do scan: ' + err.message);
       }
+      try {
+    	  var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+    	  scanner.scan(function(result) {
+            if (result.cancelled == false) {
+              callback(result.text);
+            } else {
+              Notify.alert("Notice", "Scanning cancelled.");
+            }
+          }, function(error) {
+            if (typeof errorCallback != 'undefined') {
+              errorCallback(message);
+            } else {
+              Notify.alert('Oops', 'Could not scan barcode 2: ' + error);
+            }
+          });
+        } catch(err) {
+          Notify.alert('Oops', 'Could not do scan 2: ' + err.message);
+        }
     }
 
 };
